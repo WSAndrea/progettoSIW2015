4d2e1df3b1842c8fa1ae0346b48f4cce98b71766
WSAndrea <andrewbaghi@gmail.com>
2015-06-12 01:32:24 +0200

    Sinc delle modifiche post merge

--- src/model/CustomerFacade.java
+++ src/model/CustomerFacade.java
@@ -5,11 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> refs/remotes/origin/master
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -18,7 +13,6 @@ import javax.persistence.PersistenceContext;
 public class CustomerFacade {
 
 	@PersistenceContext(unitName = "siw2015-unit")
-<<<<<<< HEAD
 	private EntityManager em;
 
 
@@ -27,20 +21,10 @@ public class CustomerFacade {
 			String day,String month,String year) {
 
 		Customer customer = new Customer();
-=======
-	private static EntityManager em;
-	
-	
-	public  Customer createCustomer(String firstName, String lastName, String email, 
-			String phoneNumber,String via,String numeroCivico,Integer cap,String state,String giornoDiNascita,String meseDiNascita,String annoDiNascita) {
-		
-		Customer customer = new Customer(); // da valutare creazione di un costruttore in Customer
->>>>>>> refs/remotes/origin/master
 		customer.setFirstName(firstName);
 		customer.setLastName(lastName);
 		customer.setEmail(email);
 		customer.setPhoneNumber(phoneNumber);
-<<<<<<< HEAD
 		Address address = new Address();
 		address.setCity(city);
 		address.setCountry(country);
@@ -50,15 +34,6 @@ public class CustomerFacade {
 		@SuppressWarnings("deprecation")
 		Date dateOfBirth = new Date(Integer.parseInt(year)-1900, Integer.parseInt(month)-1, Integer.parseInt(day));
 		customer.setDateOfBirth(dateOfBirth);
-=======
-		customer.setVia(via);
-		customer.setNumeroCivico(numeroCivico);
-		customer.setCap(cap);
-		customer.setState(state);
-		customer.setGiornoDiNascita(giornoDiNascita);
-		customer.setMeseDiNascita(meseDiNascita);
-		customer.setAnnoDiNascita(annoDiNascita);
->>>>>>> refs/remotes/origin/master
 		Date registrationDate = new Date();
 		customer.setRegistrationDate(registrationDate);
 		em.persist(customer);
@@ -86,10 +61,4 @@ public class CustomerFacade {
 			c.getOrders().add(order);
 		em.merge(c);
 	}
-
-<<<<<<< HEAD
-
-=======
-	
->>>>>>> refs/remotes/origin/master
 }
